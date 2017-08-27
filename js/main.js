a6eb70f47374500efb8c548e1df04e841116dc54
Nataliya <https.ka@gmail.com>
2017-08-27 14:49:17 +0000

    gallery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-var slideIndex = 0;
+{
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; 
     }
@@ -11,4 +12,5 @@ function showSlides() {
     if (slideIndex> slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
     setTimeout(showSlides, 4000);
+}
 }
