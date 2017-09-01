c326400e4a41b9e30e21883f0316af21d4cb1c3b
Nataliya <https.ka@gmail.com>
2017-09-01 12:07:46 +0000

    dropdown

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-{
 let slideIndex = 0;
 showSlides();
-
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 4000);
-}
-}
+    if (slideIndex > slides.length) 
+        {slideIndex = 1} 
+        slides[slideIndex-1].style.display = "block"; 
+        setTimeout(showSlides, 4000);
+    }
+    
+$(document).ready(function() {
+
+    $("#form").submit(function(){
+      $.ajax ({
+            url: "https://formspree.io/https.ka@gmail.com",
+            method: "POST",
+            dataType: "json",
+            data:$(this).serialize()
+      }).done(function(){
+            $('form')[0].reset();
+            $('#thanks').show();
+      })
+        return false;
+        });
+    });
+
+   
